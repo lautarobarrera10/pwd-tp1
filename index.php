9339ca432079f4e7f21f32deeb8cd7f4cec6e363
Lautaro Barrera <lautarobarrera12@gmail.com>
2024-08-22 18:17:07 -0300

    menú principal v1

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicios</title>
+</head>
+<body>
+    <ul>
+        <li><a href="./tp1/vistas/ej1.php">TP 1 - Ejercicio 1</a></li>
+        <li><a href="./tp1/vistas/ej2.php">TP 1 - Ejercicio 2</a></li>
+        <li><a href="./tp1/vistas/ej3.php">TP 1 - Ejercicio 3</a></li>
+        <li><a href="./tp1/vistas/ej4.php">TP 1 - Ejercicio 4</a></li>
+        <li><a href="./tp1/vistas/ej5.php">TP 1 - Ejercicio 5</a></li>
+        <li><a href="./tp1/vistas/ej6.php">TP 1 - Ejercicio 6</a></li>
+        <li><a href="./tp1/vistas/ej7.php">TP 1 - Ejercicio 7</a></li>
+        <li><a href="./tp1/vistas/ej8.php">TP 1 - Ejercicio 8</a></li>
+    </ul>
+</body>
+</html>
