3766dbdda9d70f9d9cd63a9275d206d639316e40
Lautaro Barrera <lautarobarrera12@gmail.com>
2024-08-23 11:51:46 -0300

    inputs required

--- tp2/vistas/action/action-ej-3.php
+++ tp2/vistas/action/action-ej-3.php
@@ -35,7 +35,7 @@ $usuarioCorrecto = $obj->verificarUsuario();
                             <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
                         </svg>
                     </span>
-                    <input name="usuario" id="usuario" type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                    <input name="usuario" id="usuario" type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" required>
                 </div>
                 <div class="input-group mb-3">
                     <span class="input-group-text" id="basic-addon1">
@@ -43,7 +43,7 @@ $usuarioCorrecto = $obj->verificarUsuario();
                             <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
                         </svg>
                     </span>
-                    <input name="password" id="password" type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1">
+                    <input name="password" id="password" type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1" required>
                 </div>
                 <p class="text-start" id="password-error" style="color: red;">Usuario y/o contraseña incorrectos</p>
                 <button class="btn btn-success w-100 mb-3" type="submit">Login</button>
