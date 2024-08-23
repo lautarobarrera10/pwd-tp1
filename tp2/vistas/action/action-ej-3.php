ff0f2e9037762e1c1db82b62e17d4a37ad1fa68b
Lautaro Barrera <lautarobarrera12@gmail.com>
2024-08-23 11:44:32 -0300

    ejercicio 3 terminado

---
+++ tp2/vistas/action/action-ej-3.php
@@ -0,0 +1,57 @@
+<?php
+
+include_once "../../control/UsuarioController.php";
+include_once "../../model/Usuario.php";
+include_once "../../utils/funciones.php";
+
+$datos = darDatosSubmitted();
+
+$obj = new UsuarioController($datos);
+
+$usuarioCorrecto = $obj->verificarUsuario();
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 3</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div class="d-flex align-items-center vh-100 bg-dark">
+        <?php if ($usuarioCorrecto): ?>
+            <h1 class="w-100 text-center text-light">¡Bienvenido!</h1>
+        <?php else: ?>
+            <form class="container-lg text-end shadow-lg rounded col-3 p-4 bg-light" id="login-form" action="./action-ej-3.php" method="post">
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-x-lg text-secondary text-end" viewBox="0 0 16 16">
+                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                </svg>
+                <h1 class="text-center text-secondary h2 mb-4 mt-3">Member Login</h1>
+
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon1">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                        </svg>
+                    </span>
+                    <input name="usuario" id="usuario" type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                </div>
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="basic-addon1">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                        </svg>
+                    </span>
+                    <input name="password" id="password" type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1">
+                </div>
+                <p class="text-start" id="password-error" style="color: red;">Usuario y/o contraseña incorrectos</p>
+                <button class="btn btn-success w-100 mb-3" type="submit">Login</button>
+            </form>
+        <?php endif; ?>
+    </div>
+
+    <script src="../assets/js/ej-3.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
